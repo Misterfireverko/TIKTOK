e85313344fcc5c05265e90428431e4adc012cbad
Misterfireverko <95534989+Misterfireverko@users.noreply.github.com>
2022-06-30 19:03:54 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,47 @@
+<script>
+  alert('ГЛАВНАЯ')
+alert('ТИКТОК')
+name=prompt('ТВОЙ НИК В ТТ?')
+
+name=prompt('ТВОЙ НИК В Рб?')
+
+
+password=prompt('Enter password')
+
+
+
+if (password=='1234'){
+    alert(' Password is correct')
+}
+else{
+    alert('Password is incorrect!')
+    window.close()  
+}
+
+
+password=prompt('Enter password')
+
+
+
+if (password=='4321'){
+    alert(' Password is correct')
+}
+else{
+    alert('Password is incorrect!')
+    window.close()  
+}
+
+password=prompt('Enter password')
+
+
+
+if (password=='5454'){
+    alert(' Password is correct')
+}
+else{
+    alert('Password is incorrect!')
+    window.close()  
+}
+
+alert('ДОБРО ПОЖАЛОВАТЬ '+name)
+</script>
